5a47f6b255072cecedaf8645209a5930e61d350f
YevheniiBiloshapka <106679993+YevheniiBiloshapka@users.noreply.github.com>
2023-05-23 01:33:50 +0200

    add

--- src/components/SentimentColumnChart/SentimentColumnChart.jsx
+++ src/components/SentimentColumnChart/SentimentColumnChart.jsx
@@ -19,6 +19,8 @@ const SentimentColumnChart = ({ sentimentTimeseries }) => {
 
   const chartWidth = isMobile ? 320 : isTablet ? 600 : 800;
   const chartHeight = isMobile ? 320 : isTablet ? 300 : 300;
+  const [zoomDomain, setZoomDomain] = useState(
+    isMobile ? { x: [0, 3.9] } : isTablet ? { x: [0, 6.9] } : { x: [0, 8.9] }
   );
 
   const transformData = dataset => {
