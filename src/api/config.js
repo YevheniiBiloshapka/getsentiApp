ea14c9e14f8b35cf0df7e45896788bf4af201285
Reyek <zreek2001@gmail.com>
2023-06-04 20:56:33 +0300

    minor fixes

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,1 @@
-export const REACT_APP_SERVER_URL =
-  process.env.REACT_APP_SERVER_URL || 'https://81fa-185-143-147-249.ngrok-free.app/search';
+export const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL || 'http://abc';
